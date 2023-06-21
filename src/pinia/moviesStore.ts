42fc76db79e9bd4f640089f7bbf8e0e6afc058d3
Crash2022 <crash555@mail.ru>
2023-06-21 13:19:02 +0500

    add: store Composition API example

--- src/pinia/moviesStore.ts
+++ src/pinia/moviesStore.ts
@@ -1,36 +1,67 @@
 import {defineStore} from 'pinia'
-import {SearchedMovieType} from "@/pinia/searchStore";
+import {SearchedMovieType} from "@/pinia/searchStore"
+import {ref, computed} from 'vue'
 
-export const useMoviesStore = defineStore('moviesStore', {
-    state: (): MoviesStateType => ({
-        movies: [],
-        activeTab: 'search'
-    }),
-    getters: {
-        watchedMovies(state) {
-            return state.movies.filter((m: MovieType) => m.isWatched)
-        },
-        // иная запись обращения к стейту
-        // watchedMovies(movies) {
-        //     return this.movies.filter((m: MovieType) => m.isWatched)
-        // }
+// export const useMoviesStore = defineStore('moviesStore', {
+//     state: (): MoviesStateType => ({
+//         movies: [],
+//         activeTab: 'search'
+//     }),
+//     getters: {
+//         watchedMovies(state) {
+//             return state.movies.filter((m: MovieType) => m.isWatched)
+//         },
+//         // иная запись обращения к стейту
+//         // watchedMovies(movies) {
+//         //     return this.movies.filter((m: MovieType) => m.isWatched)
+//         // }
+//
+//         // пример (чтобы достать прямое значение, нет необходимости делать геттер)
+//         // totalMoviesCount(state) {
+//         //     return state.movies.length
+//         // }
+//     },
+//     actions: {
+//         setActiveTab(activeTab: ActiveTabType) {
+//             this.activeTab = activeTab
+//         },
+//         toggleIsWatchedMovie(movieId: number) {
+//             const movieIndex = this.movies.findIndex(m => m.id === movieId)
+//             this.movies[movieIndex].isWatched = !this.movies[movieIndex].isWatched
+//         },
+//         deleteMovie(movieId: number) {
+//             this.movies = this.movies.filter(m => m.id !== movieId)
+//         }
+//     }
+// })
 
-        // пример (чтобы достать прямое значение, нет необходимости делать геттер)
-        // totalMoviesCount(state) {
-        //     return state.movies.length
-        // }
-    },
-    actions: {
-        setActiveTab(activeTab: ActiveTabType) {
-            this.activeTab = activeTab
-        },
-        toggleIsWatchedMovie(movieId: number) {
-            const movieIndex = this.movies.findIndex(m => m.id === movieId)
-            this.movies[movieIndex].isWatched = !this.movies[movieIndex].isWatched
-        },
-        deleteMovie(movieId: number) {
-            this.movies = this.movies.filter(m => m.id !== movieId)
-        }
+// вариант стора в стиле Composition API
+export const useMoviesStore = defineStore('moviesStore', () => {
+    const movies = ref([])
+    const activeTab = ref<ActiveTabType>('search')
+
+    const watchedMovies = computed(() => {
+        return movies.value.filter((m: MovieType) => m.isWatched)
+    })
+    const totalMoviesCount = computed(() => {
+        return movies.value.length
+    })
+
+    const setActiveTab = (activeTab: ActiveTabType) => {
+        activeTab.value = activeTab
+    }
+    const toggleIsWatchedMovie = (movieId: number) => {
+        const movieIndex = movies.value.findIndex((m: MovieType) => m.id === movieId)
+        movies.value[movieIndex].isWatched = !movies.value[movieIndex].isWatched
+    }
+    const deleteMovie = (movieId: number) => {
+        movies.value = movies.value.filter((m: MovieType) => m.id !== movieId)
+    }
+
+    return {
+        movies, activeTab,
+        watchedMovies, totalMoviesCount,
+        setActiveTab, toggleIsWatchedMovie, deleteMovie
     }
 })
 
--- src/pinia/searchStore.ts
+++ src/pinia/searchStore.ts
@@ -1,39 +1,67 @@
 import {defineStore} from 'pinia'
+import {ref} from 'vue'
 import {moviesAPI} from "@/shared/api/api"
 import {useMoviesStore} from "@/pinia/moviesStore"
 
-export const useSearchStore = defineStore('searchStore', {
-    state: (): SearchStateType => ({
-        isLoading: false,
-        searchedMovies: []
-    }),
-    getters: {
+// export const useSearchStore = defineStore('searchStore', {
+//     state: (): SearchStateType => ({
+//         isLoading: false,
+//         searchedMovies: []
+//     }),
+//     getters: {
+//
+//     },
+//     actions: {
+//         async getMovies(title: string) {
+//             this.isLoading = true
+//             try {
+//                 // вариант запроса без инстанс
+//                 // const response = await axios.get(`${baseURL}&query=${title}`)
+//                 // this.searchedMovies = response.data.results
+//                 // console.log(response.data.results)
+//
+//                 const response = await moviesAPI.getMovies(title)
+//                 this.searchedMovies = response
+//                 console.log(response)
+//             } catch (e) {
+//                 console.log(e)
+//             } finally {
+//                 this.isLoading = false
+//             }
+//         },
+//         addToFavorites(movie: any) {
+//             console.log(movie)
+//             const moviesStore = useMoviesStore()
+//             moviesStore.movies.push({...movie, isWatched: false})
+//             // moviesStore.activeTab = 'movies'
+//         }
+//     }
+// })
 
-    },
-    actions: {
-        async getMovies(title: string) {
-            this.isLoading = true
-            try {
-                // вариант запроса без инстанс
-                // const response = await axios.get(`${baseURL}&query=${title}`)
-                // this.searchedMovies = response.data.results
-                // console.log(response.data.results)
-
-                const response = await moviesAPI.getMovies(title)
-                this.searchedMovies = response
-                console.log(response)
-            } catch (e) {
-                console.log(e)
-            } finally {
-                this.isLoading = false
-            }
-        },
-        addToFavorites(movie: any) {
-            console.log(movie)
-            const moviesStore = useMoviesStore()
-            moviesStore.movies.push({...movie, isWatched: false})
-            moviesStore.activeTab = 'movies'
+// вариант стора в стиле Composition API
+export const useSearchStore = defineStore('searchStore', () => {
+    const isLoading = ref(false)
+    const searchedMovies = ref([])
+    const getMovies = async (title: string) => {
+        isLoading.value = true
+        try {
+            const response = await moviesAPI.getMovies(title)
+            searchedMovies.value = response
+            console.log(response)
+        } catch (e) {
+            console.log(e)
+        } finally {
+            isLoading.value = false
         }
+    }
+    const addToFavorites = (movie: any) => {
+        const moviesStore = useMoviesStore()
+        moviesStore.movies.push({...movie, isWatched: false})
+        // moviesStore.activeTab = 'movies'
+    }
+
+    return {
+        isLoading, searchedMovies, getMovies, addToFavorites
     }
 })
 
