41693fb1a41c44bf265958a35e010c96b5aa1fc5
Crash2022 <crash555@mail.ru>
2023-06-21 11:27:58 +0500

    add: HomePage image

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,12 +4,21 @@
             <h1>Добро пожаловать в приложение</h1>
             <h1>"Любимые фильмы"</h1>
         </div>
+        <div class="main_image">
+            <img :src="MainImage" alt="main-image">
+        </div>
     </div>
 </template>
 
 <script>
+import MainImage from '@/shared/assets/main-image-01.jpg'
 export default {
-
+    // импорт изображения как объект
+    data: function () {
+        return {
+            MainImage
+        }
+    }
 }
 </script>
 
@@ -19,11 +28,25 @@ export default {
     height: calc(100vh - 50px);
 
     .title {
-        margin-top: 100px;
+        margin: 30px 0;
         text-align: center;
 
         h1 {
             line-height: 50px;
+        }
+    }
+    .main_image {
+        //width: 100%;
+        text-align: center;
+
+        img {
+            // вариант с оригинальным размером изображения
+            //height: auto;
+            //max-width: 100%;
+
+            height: auto;
+            max-width: 600px;
+            width: 100%;
         }
     }
 }
